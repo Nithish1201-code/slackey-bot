d63c6fe9012f08e3a280b8e27f3c3e564def16de
Nithish1201-code <n4cubing@gmail.com>
2026-09-03 19:22:44 +0530

    added /slackey-about

--- index.js
+++ index.js
@@ -75,6 +75,14 @@ app.command("/slackey-joke", async ({ ack, respond}) => {
     }
 });
 
+app.command("/slackey-about", async ({ ack, respond}) => {
+    await ack();
+    await respond({ text: "I am Slackey, your friendly Slack bot! I can help you with various commands like pinging, rolling dice, flipping coins, telling jokes, and more!" });
+});
+
+
+
+
 (async () => {
   await app.start();
   console.log("bot is running!");
